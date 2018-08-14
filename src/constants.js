8cce5691a8aa1533f0a344e316c358bccf492df7
Itai Tenenbaum <taitulism@gmail.com>
2018-08-14 14:13:50 +0300

    remove test watch mode; clean constants file

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,6 @@
-
 export const META_TICK = 12;
 export const TIME_MARGIN = 2;
-
-// Do we have enough time to set a metaTick?
 export const META_THRESHOLD = (META_TICK * 2) + TIME_MARGIN;
-
-export const MIN_TIME_LEFT = (META_TICK / 4); // (1/4 metaTick)
-
+export const MIN_TIME_LEFT = (META_TICK / 4);
 export const ZERO = 0;
 export const TIME_PASSED = -1;
-
-// export const ONE_SECOND_THRESHOLD = ONE_SECOND + TIME_MARGIN;
-// export const MAX_SECOND_DELAY = 50;
-// export const ONE_SECOND = 1000;
-// export const HALF_A_SECOND = ONE_SECOND/2;
